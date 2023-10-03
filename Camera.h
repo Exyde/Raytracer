11a3dd6d32e268836e0fc91581ff331aa48e5194
Exyde <54008677+Exyde@users.noreply.github.com>
2023-10-03 09:11:29 +0200

    Added Camera class, cleaned main

---
+++ Camera.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "Utility.h"
+
+#include "Color.h"
+#include "Hittable.h"
+
+#include <iostream>
+
+class Camera{
+
+public:
+
+    Camera() : imgHeight(imgWidth), center(Vec3(0)), pixel00Pos(Vec3(0)), pixelDeltaU(1), pixelDeltaV(1) {}
+
+    double aspectRatio = 1.0;
+    int imgWidth = 100;
+
+    void Render(const Hittable& world){
+        Init();
+
+        //Render --
+
+        std::cout << "P3\n" << imgWidth << " " << imgHeight << "\n255\n";
+
+        for (int j = 0; j < imgHeight; ++j){
+
+            double percent  = j + 1;
+            std::cerr << "Progress : " << percent <<  "/" << imgHeight << "\n" << std::flush;
+
+            for (int i = 0; i < imgWidth; ++i){
+                
+                auto pixelCenteredPos = pixel00Pos + (i * pixelDeltaU) + (j * pixelDeltaV);
+                auto rayDirection = pixelCenteredPos - center; //Not normalized, but it's 1 for the moment
+                Ray ray(center, rayDirection);
+
+                Color pixelColor = RayColor(ray, world);
+                WriteColor(std::cout, pixelColor);
+            }
+        }
+
+        std::cerr << "\nImage rendered !\n";
+    }
+
+
+private:
+
+    int imgHeight;
+    Point center;
+    Point pixel00Pos;
+    Vec3 pixelDeltaU, pixelDeltaV;
+
+    void Init(){    
+        //Compute height, at least 1
+        imgHeight = static_cast<int>(imgWidth / aspectRatio);
+        imgHeight = ( imgHeight < 1  ) ? 1 : imgHeight;
+
+        //Virtual viewport - The viewport withint our ray will pass through 
+        auto viewportHeight = 2.0; // Arbitrary
+        auto viewportWidth = viewportHeight * (static_cast<double>(imgWidth)/ imgHeight); //Since the ratio is an approximation, we recompute it here with reals values.
+
+        //Camera
+        auto focalLength = 1.0; // Distance between eye position (camera) and the viewport. They are orthogonal.
+        auto cameraCenter = Point(0, 0, 0);
+
+        //Kind of UV's to move along the viewport - Moves only on edges ?
+        auto viewportU = Vec3(viewportWidth, 0 , 0); //That's the length, and we might lerp from 0 to length
+        auto viewportV = Vec3(0, -viewportHeight, 0);
+
+        //Compute the horizontal and vertical pixel delta (Resolution kindof)
+        pixelDeltaU = viewportU / imgWidth;
+        pixelDeltaV = viewportV / imgHeight;
+
+        //Location of upper left pixel
+        auto viewportUpperLeft = cameraCenter - Vec3(0, 0, focalLength) - viewportU / 2 - viewportV / 2; //Top Left Pos
+        pixel00Pos = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV); //Half of it, we want our pixel centered.
+    }
+
+    Color RayColor(const Ray& r, const Hittable& world){
+
+        HitInfo hit;
+        if (world.Hit(r, Interval(0, infinity), hit)){
+            return 0.5 * (hit.normal + Color(1));
+        }
+
+        Vec3 unitDirection = Normalize(r.Direction());
+
+        auto t = 0.5 * (unitDirection.y() + 1.0); //Remap coordinate to [0, 1] and use as t for lerp.
+        //Lerp : blenderValue = (1 - a) * startValue + a * endValue - SkyGradient
+        return Color (1.0 - t) * Color(1.0) + t * Color(0.5, 0.7, 1.0);
+    }
+};
--- Raytracer.cpp
+++ Raytracer.cpp
@@ -1,43 +1,15 @@
 #include "Utility.h"
 
-#include "Color.h"
-#include "Hittable.h"
+#include "Camera.h"
 #include "HittableList.h"
 #include "Sphere.h"
 
-#include <iostream>
-//Todo : Fix progress bar.
-
-Color RayColor(const Ray& r, const Hittable& world){
-
-    HitInfo hit;
-    if (world.Hit(r, Interval(0, infinity), hit)){
-        return 0.5 * (hit.normal + Color(1));
-    }
-
-
-    Vec3 unitDirection = Normalize(r.Direction());
-
-    auto t = 0.5 * (unitDirection.y() + 1.0); //Remap coordinate to [0, 1] and use as t for lerp.
-
-    //Lerp : blenderValue = (1 - a) * startValue + a * endValue - SkyGradient
-    return Color (1.0 - t) * Color(1.0) + t * Color(0.5, 0.7, 1.0);
-}
-
 int main (){
-
-    //Aspect Ratio
-    auto const ASPECT_RATIO = 16.0 / 9.0; //Ideal ratio, but not 100% accurate.
-    int imgWidth = 400;
-    int imgHeight;
-
-    //Compute height, at least 1
-    imgHeight = static_cast<int>(imgWidth / ASPECT_RATIO);
-    imgHeight = ( imgHeight < 1  ) ? 1 : imgHeight;
-
-    //Virtual viewport - The viewport withint our ray will pass through 
-    auto viewportHeight = 2.0; // Arbitrary
-    auto viewportWidth = viewportHeight * (static_cast<double>(imgWidth)/ imgHeight); //Since the ratio is an approximation, we recompute it here with reals values.
+    
+    //Camera
+    Camera cam;
+    cam.aspectRatio = 16.0 / 9.0;
+    cam.imgWidth = 400;
 
     //World
     HittableList world;
@@ -50,41 +22,6 @@ int main (){
     world.Add(make_shared<Sphere>(Point(-100.5, 0, -1), 100)); //DOWN
     */
 
-    //Camera
-    auto focalLength = 1.0; // Distance between eye position (camera) and the viewport. They are orthogonal.
-    auto cameraCenter = Point(0, 0, 0);
+   cam.Render(world);
 
-    //Kind of UV's to move along the viewport - Moves only on edges ?
-    auto viewportU = Vec3(viewportWidth, 0 , 0); //That's the length, and we might lerp from 0 to length
-    auto viewportV = Vec3(0, -viewportHeight, 0);
-
-    //Compute the horizontal and vertical pixel delta (Resolution kindof)
-    auto pixelDeltaU = viewportU / imgWidth;
-    auto pixelDeltaV = viewportV / imgHeight;
-
-    //Location of upper left pixel
-    auto viewportUpperLeft = cameraCenter - Vec3(0, 0, focalLength) - viewportU / 2 - viewportV / 2; //Top Left Pos
-    auto pixel00Pos = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV); //Half of it, we want our pixel centered.
-
-    //Render --
-
-    std::cout << "P3\n" << imgWidth << " " << imgHeight << "\n255\n";
-
-    for (int j = 0; j < imgHeight; ++j){
-
-        double percent  = j + 1;
-        std::cerr << "Progress : " << percent <<  "/" << imgHeight << "\n" << std::flush;
-
-        for (int i = 0; i < imgWidth; ++i){
-            
-            auto pixelCenteredPos = pixel00Pos + (i * pixelDeltaU) + (j * pixelDeltaV);
-            auto rayDirection = pixelCenteredPos - cameraCenter; //Not normalized, but it's 1 for the moment
-            Ray ray(cameraCenter, rayDirection);
-
-            Color pixelColor = RayColor(ray, world);
-            WriteColor(std::cout, pixelColor);
-        }
-    }
-
-    std::cerr << "\nImage rendered !\n";
 }

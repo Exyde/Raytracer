5124583ab9af7372de056474f6ef32c548f2da14
Exyde <54008677+Exyde@users.noreply.github.com>
2023-10-06 14:47:21 +0200

    Archaic preview system

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 # Previews
 ![Weird stuff](./renders/cameraPosZoomed.ppm)
 ![World 4D](./renders/world_4D.ppm)
+
+[Preview](./preview.md)
--- ppm_to_png.sh
+++ ppm_to_png.sh
@@ -9,3 +9,14 @@ convert *.ppm *.png
 echo "Converting..."
 cd ..
 echo "Conversion Done ! :] "
+
+rm preview.md
+touch preview.md
+
+files=`ls ./pngs/*.png`
+for file in $files
+do
+    echo $file
+    echo "[$file]($file)" >> preview.md 
+done
+echo "Writing preview file done !"
